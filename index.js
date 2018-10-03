03b7e5311f3cec5ac2c062721799f60ddbcb2fc1
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-03 21:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var searchRepositories = () => {
   const searchTerms = $('#searchTerms').val()
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
     $('#results').html(renderSearchResults(data))
-  }).fail(error =>{
+  }).fail(error => {
     displayError()
   })
 }
