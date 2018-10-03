01523866b842ca75c3496db95b892d1598e9e774
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-03 20:53:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 $(document).ready(function (){
 });
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $('#results').html(renderSearchResults(data))
+  }).fail(error =>{
+    displayError()
+  })
+}
